b4937f38dd40b79dca901325135db99f4d0779a9
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2024-12-10 15:38:51 +0330

    refactore(config): change static and media url for use .env

--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -165,18 +165,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = config("STATIC_URL", cast=str)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]
 
 # Media files
-
-MEDIA_URL  = os.path.join(BASE_DIR, 'media/')
-
+MEDIA_URL = config("MEDIA_URL", cast=str)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 # Default primary key field type
