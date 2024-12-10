971c150ba478ea84569c99598d3c8c318007981a
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2024-12-10 16:57:54 +0330

    Update settings.py

--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -165,14 +165,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = config("STATIC_URL", cast=str)
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]
 
 # Media files
-MEDIA_URL = config("MEDIA_URL", cast=str)
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 # Default primary key field type
